5ca25becc61be3b5c069527de4d335c33d2e8c21
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-06 12:51:32 -0800

    chore(app): add list keys for mapped elements

--- src/App.js
+++ src/App.js
@@ -26,11 +26,11 @@ const App = () => {
   ]
   return (
     <div className="categories-container">
-      {categories.map(({title}) => (
-        <div className="category-container">
-          <div className="background-image"/>
-          <div className="category-body-container">
-            <h2>{title}</h2>
+      {categories.map(({title, id}) => (
+        <div key={id} className="category-container">
+          <div  className="background-image"/>
+          <div  className="category-body-container">
+            <h2>{title} </h2>
             <p>Show Now</p>
             </div>
             </div>
