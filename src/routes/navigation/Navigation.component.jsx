c9a2659f6b7d1c52c13ceb8d053c8ecc520b50c8
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-06 16:23:15 -0800

    feat(nav): style navigation component

--- src/routes/navigation/Navigation.component.jsx
+++ src/routes/navigation/Navigation.component.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
-
+import { ReactComponent as CrwnLogo } from '../../assets/crown (1).svg';
+import './navigation.styles.scss'
 const Navigation = () => {
   return (
       <div className="navigation">
         <Link className="logoContainer" to='/'>
-        <div>Logo</div>
+        <CrwnLogo className="logo"/>
         </Link>
         <div className="navLinksContainer">
             <Link className="navLink" to='/shop'>
