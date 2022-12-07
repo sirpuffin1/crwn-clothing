324bb1213952d335af1ababb03726799f5e1b004
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-07 13:47:37 -0800

    feat(signup): style sign up form

--- src/components/sign-up-form/SignUpForm.component.jsx
+++ src/components/sign-up-form/SignUpForm.component.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import FormInput from "../form-input/FormInput.component";
+import './sign-up-form.styles.scss'
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
@@ -53,8 +54,9 @@ const SignUpForm = () => {
   };
   console.log(formFields);
   return (
-    <div>
-      <h1>Sign up with your email and password.</h1>
+    <div className="sign-up-container">
+        <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password.</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label="Display Name"
