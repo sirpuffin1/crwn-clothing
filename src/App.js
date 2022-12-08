8008c7e6f593ff682e4785c2d97ab92586d0dd00
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-08 11:13:17 -0800

    feat(nav): create/ import shop page component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./routes/home/Home.component";
 import Navigation from "./routes/navigation/Navigation.component";
 import { Routes, Route} from "react-router-dom";
 import Authentication from "./routes/authentication/Authentication.component";
+import Shop from "./routes/shop/Shop.component";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop/>} />
         <Route path="/auth" element={<Authentication/>} />
       </Routes>
     </>
---
+++ src/routes/shop/Shop.component.jsx
@@ -0,0 +1,9 @@
+const Shop = () => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default Shop;
