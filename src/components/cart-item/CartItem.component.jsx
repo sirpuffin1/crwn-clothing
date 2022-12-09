e930f362636060b5fb643fa089c2d3afba2e9e41
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-09 12:55:36 -0800

    feat(cart): display # of items, style cart

--- src/components/cart-item/CartItem.component.jsx
+++ src/components/cart-item/CartItem.component.jsx
@@ -1,11 +1,15 @@
 import './cart-item.styles.scss'
 
 const CartItem = ({cartItem}) => {
-    const {name, quantity} = cartItem
+    const {name, quantity, imageUrl, price} = cartItem
     return (
-        <div>
-            <h2>{name}</h2>
-            <span>{quantity}</span>
+        <div className='cart-item-container'>
+            <img src={imageUrl} alt={name}/>
+            <div className='item-details'>
+            <h2 className='name'>{name}</h2>
+            <span className='price'>{quantity} x ${price}</span>
+            </div>
+           
         </div>
     );
 }
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -16,21 +16,26 @@ export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
-    addItemToCart: () => {}
+    addItemToCart: () => {},
+    cartCount: 0
 })
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([])
+    const [cartCount, setCartCount] = useState(0)
+
+    const cartItemsNumber = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
 
     const addItemToCart = (productToAdd) => {
-        setCartItems(addCartItem(cartItems, productToAdd))
-       
-
-        
+        setCartItems(addCartItem(cartItems, productToAdd))        
     }
 
-    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems};
+    useEffect(() => {
+        setCartCount(cartItemsNumber)
+    },[cartItems])
+
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartCount, cartItems};
 
     return <CartContext.Provider value={value}>
         { children }
