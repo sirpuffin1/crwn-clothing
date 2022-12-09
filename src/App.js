b188df55b415067acee4438b717859aa3346bf5c
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-09 13:03:51 -0800

    feat(checkout): create/import checkout route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from "./routes/navigation/Navigation.component";
 import { Routes, Route} from "react-router-dom";
 import Authentication from "./routes/authentication/Authentication.component";
 import Shop from "./routes/shop/Shop.component";
+import Checkout from "./routes/checkout/Checkout.component";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop/>} />
         <Route path="/auth" element={<Authentication/>} />
+        <Route path="/checkout" element={<Checkout/>} />
       </Routes>
     </>
   );
---
+++ src/routes/checkout/Checkout.component.jsx
@@ -0,0 +1,11 @@
+import './checkout.styles.scss'
+
+const Checkout = () => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default Checkout;
