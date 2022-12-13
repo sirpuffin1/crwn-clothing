11463bf9371d0808028e372fbdc87097af25fed2
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-13 12:16:50 -0800

    feat(DirectoryItem): convert to styled component

--- src/components/directory-item/DirectoryItem.jsx
+++ src/components/directory-item/DirectoryItem.jsx
@@ -1,17 +1,15 @@
-import './directory-item.styles.scss'
+import {DirectoryItemContainer, Body, BackgroundImage} from './directory-item.styles'
 
 const DirectoryItem = ({category}) => {
     const {title, imageUrl} = category
     return (
-        <div className="directory-item-container">
-          <div  className="background-image" style={{
-            backgroundImage: `url(${imageUrl})`
-          }}/>
-          <div  className="directory-item-body">
+        <DirectoryItemContainer>
+          <BackgroundImage  imageUrl={imageUrl} />
+          <Body>
             <h2>{title} </h2>
             <p>Show Now</p>
-            </div>
-            </div>
+            </Body>
+            </DirectoryItemContainer>
     );
 }
 
