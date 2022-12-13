33842a7d1b1ea9304c25ccff1f94bbc07040b4a3
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-13 10:39:59 -0800

    feat(shop): add links to respective category pages

--- src/components/category-preview/CategoryPreview.component.jsx
+++ src/components/category-preview/CategoryPreview.component.jsx
@@ -1,11 +1,14 @@
 import "./category-preview.styles.scss";
 import ProductCard from "../product-card/ProductCard.component";
+import { Link } from "react-router-dom";
 
 const CategoryPreview = ({ title, products }) => {
   return (
     <div className="category-preview-container">
       <h2>
-        <span className="title">{title.toUpperCase()}</span>
+        <Link className="title" to={title}>
+       {title.toUpperCase()}
+        </Link>
       </h2>
       <div className="preview">
         {products.filter((_, index) => index < 4).map((product) => (
