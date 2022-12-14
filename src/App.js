23a5cc0232988e7063c45ac8bcb730a69e7b2c93
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-14 14:33:32 -0800

    feat(app): use redux flow to dispatch set action

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import Home from "./routes/home/Home.component";
 import Navigation from "./routes/navigation/Navigation.component";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Authentication from "./routes/authentication/Authentication.component";
 import Shop from "./routes/shop/Shop.component";
 import Checkout from "./routes/checkout/Checkout.component";
+import { setCurrentUser } from "./store/user/user.action";
+
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import {
+  createUserDocumentFromAuth,
+  onAuthStateChangedListener,
+} from "./utils/firebase/firebase.utils";
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe;
+  }, [dispatch]);
+
   return (
     <>
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/shop/*" element={<Shop/>} />
-        <Route path="/auth" element={<Authentication/>} />
-        <Route path="/checkout" element={<Checkout/>} />
+        <Route path="/shop/*" element={<Shop />} />
+        <Route path="/auth" element={<Authentication />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </>
   );
