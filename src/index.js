7428ca24ca148ad7f23e17efdca49bbec95b5403
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-14 11:37:54 -0800

    feat(store): create and import store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import "./index.scss";
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
 import reportWebVitals from "./reportWebVitals";
 import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
+import { store } from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
       <UserProvider>
           <CategoriesProvider>
@@ -20,6 +23,7 @@ root.render(
           </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,9 @@
-import { compose, createStore, applyMiddleware} from 'redux'
+import { compose, legacy_createStore as createStore, applyMiddleware} from 'redux'
 import logger from 'redux-logger'
 
-//root-reducer
+import { rootReduer } from './root-reducer'
+
+const middleWares = [logger]
+
+const composedEnhancers = compose(applyMiddleware(...middleWares))
+export const store = createStore(rootReduer, undefined, composedEnhancers)
