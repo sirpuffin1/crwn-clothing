07fd202c104df35119f2c4b6ed9e24b4663c7214
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-13 16:55:27 -0800

    ref(cart): toggle cart display on navigation

--- src/components/cart-dropdown/CartDropdown.component.jsx
+++ src/components/cart-dropdown/CartDropdown.component.jsx
@@ -6,11 +6,11 @@ import { CartContext } from "../../contexts/cart.context";
 import { useNavigate } from "react-router-dom";
 
 const CartDropdown = () => {
-  const { cartItems, toggleIsCartOpen } = useContext(CartContext);
+  const { cartItems, setIsCartOpen } = useContext(CartContext);
   const navigate = useNavigate();
   const goToCheckout = () => {
     navigate("/checkout");
-    toggleIsCartOpen();
+    setIsCartOpen(false)
   };
 
   return (
