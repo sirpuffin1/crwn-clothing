9cc2f756b417c7b582a0fa53b785969bc331c837
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-14 14:32:34 -0800

    ref(app): remove user provider

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import "./index.scss";
 import App from "./App";
-import { UserProvider } from "./contexts/user.context";
 import reportWebVitals from "./reportWebVitals";
 import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
@@ -14,15 +13,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
-      <UserProvider>
-          <CategoriesProvider>
+      <BrowserRouter>
+        <CategoriesProvider>
           <CartProvider>
             <App />
-            </CartProvider>
-          </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+          </CartProvider>
+        </CategoriesProvider>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
