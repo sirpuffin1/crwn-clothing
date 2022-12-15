dff406adcf8d0329cef3ce1611de6680475c44b7
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-15 12:51:50 -0800

    feat(redux): utilize memoized selector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,9 +1,21 @@
-export const selectCategoriesMap = (state) => {
-  console.log('selector fired')
+import { createSelector } from "reselect";
 
-  return state.category.categories.reduce((acc, category) => {
-    const {title, items } = category
-    acc[title.toLowerCase()] = items
-    return acc
-  }, {})
-}
+const selectCategoryReducer = (state) => state.category;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categorySlice) => categorySlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => {
+    console.log('category selector fired')
+
+    return categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+  }
+);
