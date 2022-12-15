f76ca54f8dbf0e386949c43c6414d1430ec801a7
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-14 16:33:07 -0800

    feat(redux): create category boiler plate

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
 import { userReducer } from './user/user.reducer';
+import { categoriesReducer } from './categories/category.reducer';
 
 export const rootReduer = combineReducers({
-    user: userReducer
+    user: userReducer,
+    category: categoriesReducer
 })
