0854252d97983eb7c338e59de855710957b7c786
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-21 10:21:04 -0800

    feat(category): type actions

---
+++ src/store/categories/category.action.ts
@@ -0,0 +1,36 @@
+import {
+  createAction,
+  Action,
+  ActionWithPayload,
+} from "../../utils/reducer/reducer.utils";
+import { CATEGORIES_ACTION_TYPE, Category } from "./category.types";
+
+export type FetchCategoriesStart =
+  Action<CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START>;
+export type FetchCategoriesSuccess = ActionWithPayload<
+  CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_SUCCESS,
+  Category[]
+>;
+export type FetchCategoriesFailed = ActionWithPayload<
+  CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAILED,
+  Error
+>;
+
+export type CategoryAction =
+  | FetchCategoriesStart
+  | FetchCategoriesSuccess
+  | FetchCategoriesFailed;
+
+export const fetchCategoriesStart = (): FetchCategoriesStart =>
+  createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (
+  categoriesArray: Category[]
+): FetchCategoriesSuccess =>
+  createAction(
+    CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_SUCCESS,
+    categoriesArray
+  );
+
+export const fetchCategoriesFailed = (error: Error): FetchCategoriesFailed =>
+  createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAILED, error);
