cdff8de46cc62870993c3b17b583a59f706d440e
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-21 14:38:34 -0800

    feat(category): use new fn to wrap actions

--- src/store/categories/category.action.ts
+++ src/store/categories/category.action.ts
@@ -2,35 +2,33 @@ import {
   createAction,
   Action,
   ActionWithPayload,
+  withMatcher
 } from "../../utils/reducer/reducer.utils";
 import { CATEGORIES_ACTION_TYPE, Category } from "./category.types";
 
 export type FetchCategoriesStart =
   Action<CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START>;
+
 export type FetchCategoriesSuccess = ActionWithPayload<
   CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_SUCCESS,
   Category[]
 >;
+
 export type FetchCategoriesFailed = ActionWithPayload<
   CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAILED,
   Error
 >;
 
-export type CategoryAction =
-  | FetchCategoriesStart
-  | FetchCategoriesSuccess
-  | FetchCategoriesFailed;
+export const fetchCategoriesStart = withMatcher((): FetchCategoriesStart =>
+  createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START));
 
-export const fetchCategoriesStart = (): FetchCategoriesStart =>
-  createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START);
-
-export const fetchCategoriesSuccess = (
+export const fetchCategoriesSuccess = withMatcher((
   categoriesArray: Category[]
 ): FetchCategoriesSuccess =>
   createAction(
     CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_SUCCESS,
     categoriesArray
-  );
+  ));
 
-export const fetchCategoriesFailed = (error: Error): FetchCategoriesFailed =>
-  createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAILED, error);
+export const fetchCategoriesFailed = withMatcher((error: Error): FetchCategoriesFailed =>
+  createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAILED, error));
