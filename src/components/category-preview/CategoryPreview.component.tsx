bc2b03e67480ae4127bb901e020e880f5473d7b2
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-22 14:16:16 -0800

    ref(CategoryPreview): type component

--- src/components/category-preview/CategoryPreview.component.tsx
+++ src/components/category-preview/CategoryPreview.component.tsx
@@ -1,8 +1,15 @@
 import "./category-preview.styles.scss";
 import ProductCard from "../product-card/ProductCard.component";
 import { Link } from "react-router-dom";
+import { FC } from "react";
+import { CategoryItem } from "../../store/categories/category.types";
 
-const CategoryPreview = ({ title, products }) => {
+type CategoryPreviewProps = {
+  title: string;
+  products: CategoryItem[]
+}
+
+const CategoryPreview: FC<CategoryPreviewProps> = ({ title, products }) => {
   return (
     <div className="category-preview-container">
       <h2>
