fd57705110846f3c137fc34f0524ffd1d56faa2d
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-22 14:34:08 -0800

    ref(ProductCard): type component

--- src/components/product-card/ProductCard.component.tsx
+++ src/components/product-card/ProductCard.component.tsx
@@ -3,8 +3,15 @@ import Button, {BUTTON_TYPES_CLASSES} from '../button/Button.component';
 import { addItemToCart} from "../../store/cart/cart.action";
 import {selectCartItems} from "../../store/cart/cart.selector"
 import { useDispatch, useSelector } from "react-redux";
+import { FC } from 'react';
+import { CategoryItem } from '../../store/categories/category.types';
 
-const ProductCard = ({product}) => {
+
+type ProductCardProps = {
+    product: CategoryItem;
+}
+
+const ProductCard: FC<ProductCardProps> = ({product}) => {
     const {name, price, imageUrl} = product;
     const dispatch = useDispatch();
     const cartItems = useSelector(selectCartItems)
