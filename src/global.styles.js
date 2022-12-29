cf325d64103a73b3214931ec617dcc1a4667fdc8
sirpuffin1 <angelcruz981@yahoo.com>
2022-12-29 10:56:54 -0800

    chore(styles): add mobile responsive styles

--- src/global.styles.js
+++ src/global.styles.js
@@ -17,4 +17,18 @@ export const GlobalStyle = createGlobalStyle`
     * {
         box-sizing: border-box;
     }
+
+    body {
+            margin: 0;
+            font-family: 'Open Sans', sans-serif;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            font-stretch: condensed;
+            padding: 20px 40px;
+            }
+
+        code {
+            font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+                monospace;
+        }
 `;
